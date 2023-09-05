989628043fb65fc9ecd62eee7fc336733e8ac22c
isCatNull <90348097+isCatNull@users.noreply.github.com>
2023-09-05 22:30:34 +0100

    Improve naming

--- VersionIncrementer/Application/Services/IncrementerService.cs
+++ VersionIncrementer/Application/Services/IncrementerService.cs
@@ -6,39 +6,37 @@ public class IncrementerService : IIncrementerService
 {
     public string UpdateMinorVersion(string version)
     {
-        var versionArray = version.Split('.');
-        var minorVersion = versionArray[3];
-        var updatedNumericVersion = int.Parse(minorVersion) + 1;
+        var versionCharacters = version.Split('.');
+        var minorVersion = versionCharacters[3];
+        var incrementedVersion = int.Parse(minorVersion) + 1;
 
         var stringBuilder = new StringBuilder();
 
         for (int i = 0; i < 3; i++)
         {
-            stringBuilder.Append(versionArray[i]);
+            stringBuilder.Append(versionCharacters[i]);
             stringBuilder.Append('.');
         }
 
-        stringBuilder.Append(updatedNumericVersion);
+        stringBuilder.Append(incrementedVersion);
 
         return stringBuilder.ToString();
     }
 
     public string UpdateMajorVersion(string version)
     {
-        var versionArray = version.Split('.');
-
-        var majorVersion = versionArray[2];
-
-        var numericMajorVersion = int.Parse(majorVersion) + 1;
+        var versionCharacters = version.Split('.');
+        var majorVersion = versionCharacters[2];
+        var incrementedVersion = int.Parse(majorVersion) + 1;
 
         var stringBuilder = new StringBuilder();
 
         for (int i = 0; i < 2; i++)
         {
-            stringBuilder.Append(versionArray[i]);
+            stringBuilder.Append(versionCharacters[i]);
             stringBuilder.Append('.');
         }
-        stringBuilder.Append(numericMajorVersion);
+        stringBuilder.Append(incrementedVersion);
         stringBuilder.Append(".0");
 
         return stringBuilder.ToString();
